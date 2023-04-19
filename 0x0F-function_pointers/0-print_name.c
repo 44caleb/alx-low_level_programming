65cec737e564c36697238cb3a246100aca528892
44caleb <calebadinfono2@gmail.com>
2023-04-19 14:57:30 +0100

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
  * print_name - prints a name
  * @name: name of person
  * @f: pointer to function that actually prints the name
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
