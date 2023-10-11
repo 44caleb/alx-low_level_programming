08ec77b91dea6466710adfdb4459a7d571994c44
44caleb <calebadinfono2@gmail.com>
2023-10-11 16:13:14 -0700

    search algo 2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,12 @@
 #include "search_algos.h"
 
-
+/**
+ * binary_search - binary search algorithm
+ * @array: array to be searched
+ * @size: number of elements
+ * @value: number to be searched for
+ * Return: index or -1 if number not found
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0;
@@ -15,7 +21,14 @@ int binary_search(int *array, size_t size, int value)
 }
 
 
-
+/**
+ * recursive_search - recurisve search func
+ * @left: leftmost index position
+ * @right: rightmost index position
+ * @array: array to be searched
+ * @value: number to be searched for
+ * Return: index of number, -1 if not found
+ */
 int recursive_search(size_t left, size_t right, int *array, int value)
 {
 	size_t i, mid;
